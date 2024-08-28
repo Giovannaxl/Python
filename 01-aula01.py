8ac2ddffbcc993f0116b2c7b60e5b3a3b7171151
Gigi <gio63745@gmail.com>
2024-08-28 17:26:48 -0300

    Add files via upload

---
+++ 01-aula01.py
@@ -0,0 +1,19 @@
+# Aqui eu posso adicionar comentários
+# Continuar na linha de baixo
+print("Giovanna") # Comentário a linha de código
+print('Gio')
+print('Giovanna "Gigi" Andrade')
+print("Giovanna 'Gigi' Andrade") 
+print("Giovanna\"Gigi\"Andrade")
+
+teste = "oi"
+print(type(teste))
+teste = 17
+print(teste)
+print(type(teste))
+teste = 12.1
+print(teste)
+print(type(teste))
+teste = False
+print(teste)
+print(type(teste))
