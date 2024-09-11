2af1b352f21530a9acfe29e693fd45402f684280
Gigi <gio63745@gmail.com>
2024-09-11 16:42:20 -0300

    Add files via upload

---
+++ 03-aula03.py
@@ -0,0 +1,35 @@
+# Tipos de Dados Pimitivos:
+# - inteiro (int): númeors inteiros
+# - Float (float): némeros reais, decimais
+# - String (str): cadeia de caracteres, utilizando aspas
+# - Boolean (bool): tipo de lógico True ou False
+# - Complex (complex): números complexos, com parte real e imaginária
+# - list (list): lista de elementos, ordenados e indexados
+# - Tuple (tuple): tupla de elementos, ordenados e imutáveis
+# - Dictionary (dict): dicionário de elementos, não ordenados e indexados
+
+#
+nome = input("Digite o seu nome: ")
+print(type(nome))
+idade = input("Digite: ")
+print(type(idade))
+altura = input("Digite: ")
+print(type(altura))
+
+# #Exibir na tela
+ print(f"nome: {nome} idade: {idade} altura: {altura}")
+
+ 
+ #Exemplo de convensão de string para inteiro
+valorA = int(input("Digite o valor de A: "))
+valorB = int(input("Digite o valor de B: "))
+resultador = valorA + valorB
+print(f"A soma = {resultado}")
+
+# Exemplo de conversão de string para float
+valorA = float(input("Digite o valor de A:"))
+valorB = float(input("Digite o valor de B:"))
+resultado = valorA + valorB
+print(f"A soma = {resultado}")
+ 
+
