7ec7cc03c92aa420c9a291fc0392c35c03966796
Gigi <gio63745@gmail.com>
2024-09-11 13:54:13 -0300

    Add files via upload

---
+++ 02-aula02.py
@@ -0,0 +1,9 @@
+# Atribuição de variáveis
+nome = "Giovanna"
+idade = "16"
+altura = "1.58"
+
+# exibir mensagem na tela
+print(" nome: ", nome, "idade: ", idade, "altura: ", idade ,)
+#Formatação com f-string
+print(f"nome: {nome} idade: {idade} altura: {altura}")
