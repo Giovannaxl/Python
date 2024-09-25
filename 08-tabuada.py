da43b5920ddd9082ab8d830aa866fe3601aef47c
Gigi <gio63745@gmail.com>
2024-09-25 15:43:00 -0300

    Add files via upload

---
+++ 08-tabuada.py
@@ -0,0 +1,24 @@
+
+
+
+#Variáveis
+contador = 0
+multiplicador = int(input("Digite o multiplicador: "))
+resultado = 0
+
+
+while contador <=10:
+    resultado = multiplicador * contador
+    print(f"{multiplicador} x {contador} = {resultado}")
+    contador +=1
+
+   
+ 
+    
+
+
+    
+
+     
+     
+    
