ed5474e8a7e285466f397689b4d6a8e21d33bf8c
Gigi <gio63745@gmail.com>
2024-09-26 18:26:23 -0300

    Add files via upload

---
+++ 12-tabuada1ao10.py
@@ -0,0 +1,18 @@
+print("Tabuada do 1 ao 10: \n")
+
+
+contador = 0
+multiplicador = 1
+resultado = 0
+
+while multiplicador <=10:
+    contador = 0
+    print(f"Tabuada do {multiplicador}")
+    while contador <=10:
+        resultado = multiplicador * contador
+        print(f"{multiplicador} x {contador} = {resultado}")
+        contador +=1
+    multiplicador +=1
+    print()
+
+    
