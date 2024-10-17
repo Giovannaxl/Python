24ddc28b85051ea641fd00e9ea03e51b8668d9b8
Gigi <gio63745@gmail.com>
2024-10-17 17:35:29 -0300

    Add files via upload

---
+++ 24-soma-pares-for.py
@@ -0,0 +1,9 @@
+#Giovanna de Andrade
+
+#Declaração das váriaveis
+soma = 0
+print("Soma dos pares entre 1 e 50: ")
+for numero in range (1,51):
+    if numero %2 == 0: #Verifica se o número é par
+        soma += numero #Adiciona o numero à soma
+        print(f"A soma dos números pares entre 1 e 50 é: {soma}")
